b809f201b279eabb277b88d5928f3dede8dd78d7
corselia <takiya@toran.sakura.ne.jp>
2019-07-06 20:09:31 +0900

    Write a minimum spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   # 姓、名、メール、パスワードがあれば有効な状態であること
-  it 'is valid with a first name, last name, email, and password'
+  it 'is valid with a first name, last name, email, and password' do
+    user = User.new(
+      first_name: 'Aaron',
+      last_name: 'Sumner',
+      email: 'tester@exapmple.com',
+      password: 'dottle-nouveau-pavilion-tights-fuzure'
+    )
+    expect(user).to be_valid
   end
 
   # 「名」がなければ無効な状態であること
