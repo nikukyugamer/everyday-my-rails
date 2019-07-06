1d1c43ae22fbe7056baaea4936b177f34a070d21
corselia <takiya@toran.sakura.ne.jp>
2019-07-06 22:24:58 +0900

    Substitute specs with FactoryBot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,21 +20,21 @@ RSpec.describe User, type: :model do
 
   # 「名」がなければ無効な状態であること
   it 'is invalid without a first name' do
-    user = User.new(first_name: nil)
+    user = FactoryBot.build(:user, first_name: nil)
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
 
   # 「姓」がなければ無効な状態であること
   it 'is invalid without a last name' do
-    user = User.new(last_name: nil)
+    user = FactoryBot.build(:user, last_name: nil)
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
 
   # メールアドレスがなければ無効な状態であること
   it 'is invalid without an email address' do
-    user = User.new(email: nil)
+    user = FactoryBot.build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
