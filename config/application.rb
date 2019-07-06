86ad272c531443f893bc2c088cf17e5f1ba85bea
corselia <takiya@toran.sakura.ne.jp>
2019-07-06 14:21:29 +0900

    Refactor a bit

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,10 @@
 require_relative 'boot'
-
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Projects
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
     config.generators do |g|
