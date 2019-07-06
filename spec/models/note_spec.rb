f9f904c96ecc5e82fd3e1435bc29624b7160f056
corselia <takiya@toran.sakura.ne.jp>
2019-07-06 21:27:50 +0900

    Use 'context'

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,69 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  # 検索文字列に一致するメモを返すこと
-  it 'returns notes that match the search term' do
-    user = User.create(
-      first_name: 'Joe',
-      last_name: 'Tester',
-      email: 'joetester@example.com',
-      password: 'dottle-nouveau-pavilion-tights-fuzure',
-    )
+  # 文字列に一致するメッセージを検索する
+  describe 'search message for a term' do
+    # 一致するデータが見つかるとき
+    context 'when a match is found' do
+      # 検索文字列に一致するメモを返すこと
+      it 'when a match is found' do
+        user = User.create(
+          first_name: 'Joe',
+          last_name: 'Tester',
+          email: 'joetester@example.com',
+          password: 'dottle-nouveau-pavilion-tights-fuzure',
+        )
 
-    project = user.projects.create(
-      name: 'Test Project',
-    )
+        project = user.projects.create(
+          name: 'Test Project',
+        )
 
-    note1 = project.notes.create(
-      message: 'This is the first note.',
-      user: user,
-    )
+        note1 = project.notes.create(
+          message: 'This is the first note.',
+          user: user,
+        )
 
-    note2 = project.notes.create(
-      message: 'This is the second note.',
-      user: user,
-    )
+        note2 = project.notes.create(
+          message: 'This is the second note.',
+          user: user,
+        )
 
-    note3 = project.notes.create(
-      message: 'First, preheat the oven.',
-      user: user
-    )
+        note3 = project.notes.create(
+          message: 'First, preheat the oven.',
+          user: user
+        )
 
-    # search スコープ は 検索文字列 に一致するメモのコレクションを返す (downcase)
-    expect(Note.search('first')).to include(note1, note3)
-    expect(Note.search('first')).to_not include(note2)
-  end
+        # search スコープ は 検索文字列 に一致するメモのコレクションを返す (downcase)
+        expect(Note.search('first')).to include(note1, note3)
+        expect(Note.search('first')).to_not include(note2)
+      end
+    end
 
-  # 検索結果が一見も見つからなければ空のコレクションを返すこと
-  it 'returns an empty collection when no results are found' do
-    user = User.create(
-      first_name: 'Joe',
-      last_name: 'Tester',
-      email: 'joetester@example.com',
-      password: 'dottle-nouveau-pavilion-tights-furze',
-    )
+    # 一致するデータが 1件 も見つからないとき
+    context 'when no match is found' do
+      # 空のコレクションを返すこと
+      it 'returns an empty collection when no results are found' do
+        user = User.create(
+          first_name: 'Joe',
+          last_name: 'Tester',
+          email: 'joetester@example.com',
+          password: 'dottle-nouveau-pavilion-tights-furze',
+        )
 
-    project = user.projects.create(
-      name: 'Test Project',
-    )
+        project = user.projects.create(
+          name: 'Test Project',
+        )
 
-    # rubocop:disable Lint/UselessAssignment
-    note1 = project.notes.create(
-      message: 'This is the first note.',
-      user: user,
-    )
+        # rubocop:disable Lint/UselessAssignment
+        note1 = project.notes.create(
+          message: 'This is the first note.',
+          user: user,
+        )
 
-    note2 = project.notes.create(
-      message: 'This is the second note.',
-      user: user,
-    )
+        note2 = project.notes.create(
+          message: 'This is the second note.',
+          user: user,
+        )
 
-    note3 = project.notes.create(
-      message: 'First, preheat the oven.',
-      user: user,
-    )
-    # rubocop:enable Lint/UselessAssignment
+        note3 = project.notes.create(
+          message: 'First, preheat the oven.',
+          user: user,
+        )
+        # rubocop:enable Lint/UselessAssignment
 
-    expect(Note.search('message')).to be_empty
+        expect(Note.search('message')).to be_empty
+      end
+    end
   end
 end
