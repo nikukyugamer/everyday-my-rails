3ff483f28eca5524a8ef50f0de48b32fc14ba845
corselia <takiya@toran.sakura.ne.jp>
2019-07-06 20:22:52 +0900

    Implement a spec about last_name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,7 +20,11 @@ RSpec.describe User, type: :model do
   end
 
   # 「姓」がなければ無効な状態であること
-  it 'is invalid without a last name'
+  it 'is invalid without a last name' do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
 
   # メールアドレスがなければ無効な状態であること
   it 'is invalid without an email address'
