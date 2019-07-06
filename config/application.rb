7c63ce1cfd6c5596db7434d26aab8be073633b17
corselia <takiya@toran.sakura.ne.jp>
2019-07-06 22:12:58 +0900

    Valid a using factory part in application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module Projects
 
     config.generators do |g|
       g.test_framework :rspec,
-                       fixtures: false, # テストデータベースにレコードを作成するファイルの作成をスキップ（FactoryBot を使うため）
                        view_specs: false, # ビュースペックは用いずに、さらにフィーチャースペックも用いずに、システムスペックを用いる
                        helper_specs: false, # ヘルパファイルは現時点ではテストしない
                        routing_specs: false # ルーティングスペックは現時点ではテストしない（アプリが大きくなったら考える）
