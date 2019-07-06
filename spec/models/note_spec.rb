686057b60b264c30cc40648537438de00faa628e
corselia <takiya@toran.sakura.ne.jp>
2019-07-06 20:54:55 +0900

    Implement a spec about Note's 'search' scope whether returns correct results

---
+++ spec/models/note_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Note, type: :model do
+  # 検索文字列に一致するメモを返すこと
+  it 'returns notes that match the search term' do
+    user = User.create(
+      first_name: 'Joe',
+      last_name: 'Tester',
+      email: 'joetester@example.com',
+      password: 'dottle-nouveau-pavilion-tights-fuzure',
+    )
+
+    project = user.projects.create(
+      name: 'Test Project',
+    )
+
+    note1 = project.notes.create(
+      message: 'This is the first note.',
+      user: user,
+    )
+
+    note2 = project.notes.create(
+      message: 'This is the second note.',
+      user: user,
+    )
+
+    note3 = project.notes.create(
+      message: 'First, preheat the oven.',
+      user: user
+    )
+
+    # search スコープ は 検索文字列 に一致するメモのコレクションを返す (downcase)
+    expect(Note.search('first')).to include(note1, note3)
+    expect(Note.search('first')).to_not include(note2)
+  end
+end
